e3c66ff23e9007a677a6b8f40c7219ca39d43344
Jigyasa Kaushik <jkaushik0802@gmail.com>
2024-05-30 20:25:31 +0530

    Dashboard view created

---
+++ src/components/BarChart.vue
@@ -0,0 +1,49 @@
+<template>
+    <canvas ref="barChart"></canvas>
+  </template>
+  
+  <script>
+  import { onMounted, ref } from 'vue';
+  import {
+    Chart,
+    BarController,
+    BarElement,
+    LinearScale,
+    Title,
+    CategoryScale
+  } from 'chart.js';
+  
+  Chart.register(BarController, BarElement, LinearScale, Title, CategoryScale);
+  
+  export default {
+    name: 'BarChart',
+    setup() {
+      const barChart = ref(null);
+  
+      onMounted(() => {
+        const ctx = barChart.value.getContext('2d');
+        new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            datasets: [{
+              label: 'Recent Marketing',
+              data: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60],
+              backgroundColor: 'rgba(153, 102, 255, 0.6)',
+              borderColor: 'rgba(153, 102, 255, 1)',
+              borderWidth: 1,
+            }]
+          },
+          options: {
+            responsive: true,
+          }
+        });
+      });
+  
+      return {
+        barChart
+      };
+    }
+  }
+  </script>
+  
---
+++ src/components/LineChart.vue
@@ -0,0 +1,81 @@
+<template>
+  <canvas ref="lineChart"></canvas>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import {
+  Chart,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale,
+} from "chart.js";
+
+Chart.register(
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale
+);
+
+export default {
+  name: "LineChart",
+  setup() {
+    const lineChart = ref(null);
+
+    onMounted(() => {
+      const ctx = lineChart.value.getContext("2d");
+      new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+          ],
+          datasets: [
+            {
+              label: "Recent Workflow",
+              data: [12, 19, 3, 5, 2, 3, 10, 15, 20, 25, 30, 35],
+              borderColor: "rgba(75, 192, 192, 1)",
+              borderWidth: 1,
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+        },
+      });
+    });
+
+    return {
+      lineChart,
+    };
+  },
+};
+</script>
+
+<style scoped>
+canvas {
+  flex: 1 1 45%;
+  min-width: 300px;
+  height: 200px;
+  background-color: white;
+  border-radius: 5px;
+}
+</style>
